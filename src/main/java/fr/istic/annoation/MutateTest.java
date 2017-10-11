c4d560983490897c82c027ec1756c09a89fe76d9
Unknown <romaindwan@gmail.com>
2017-10-11 11:16:38 +0200

    ajout de l'annotation1

---
+++ src/main/java/fr/istic/annoation/MutateTest.java
@@ -0,0 +1,26 @@
+package fr.istic.annoation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+/**
+ * This annotation will be used to indicate if a Type or a methode have to mutated
+ * @author rostom
+ *
+ */
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MutateTest {
+	
+	/**
+	 * the default value is false, means that the mutation will be occured
+	 * @return 
+	 */
+    boolean ignor() default false;
+
+}
