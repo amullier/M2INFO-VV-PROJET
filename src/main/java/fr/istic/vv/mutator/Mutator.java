79c96bb5447102a8f89799b606bc22c4fb4fbd33
Unknown <romaindwan@gmail.com>
2017-11-19 15:01:56 +0100

    make mutation into the class itself.instead of create a new copy class

--- src/main/java/fr/istic/vv/mutator/Mutator.java
+++ src/main/java/fr/istic/vv/mutator/Mutator.java
@@ -3,6 +3,7 @@ package fr.istic.vv.mutator;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Enumeration;
+import java.util.List;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
@@ -11,30 +12,33 @@ import org.slf4j.LoggerFactory;
 
 import fr.istic.vv.mutator.projet.MutateClass;
 import fr.istic.vv.mutator.projet.MutateMethod;
-import javassist.CannotCompileException;
+import fr.istic.vv.testrunner.runner.TestRunner;
 import javassist.ClassPool;
 import javassist.CtClass;
-import javassist.CtField;
 import javassist.CtMethod;
-import javassist.NotFoundException;
 import javassist.bytecode.BadBytecode;
 import javassist.bytecode.Bytecode;
 import javassist.bytecode.ClassFile;
 import javassist.bytecode.CodeAttribute;
 import javassist.bytecode.CodeIterator;
 import javassist.bytecode.DuplicateMemberException;
-import javassist.bytecode.FieldInfo;
-import javassist.bytecode.MethodInfo;
 import javassist.bytecode.Mnemonic;
 
 public class Mutator {
 
 	private static Logger logger = LoggerFactory.getLogger(Mutator.class);
+	private List<Class> classes;
+	private TestRunner testRunner;
+	
+	public Mutator(List<Class> classes, TestRunner testRunner) {
+		this.classes = classes;
+		this.testRunner = testRunner;
+	}
 	
 	public void mutate() throws Exception {
 		String jarFile = "../M2INFO-VV-DUMMY-PROJET/target/VV-DUMMY-PROJET-1.0-SNAPSHOT.jar";
 		JarFile jar = new JarFile(jarFile);
-
+		
 		Enumeration<JarEntry> jarEntries = jar.entries();
 
 		while (jarEntries.hasMoreElements()) {
@@ -57,44 +61,24 @@ public class Mutator {
 					}
 				}
 				
-				System.out.println(ctClass.getName());
 				if(!ctClass.isInterface()) {
 					// faudrait utilisé le cf ??
+					ClassFile cf = ctClass.getClassFile();
 					
-					// /!\ ici on construit la class
-					ClassFile cf = initClassFile(ctClass);
 					Bytecode code = new Bytecode(cf.getConstPool());
 					
 					// construction de la class a muter ??????????????? pas très sur de son utilité ........
 					MutateClass mc = new MutateClass(ctClass.getName());
 					
-					CtField[] fields = ctClass.getDeclaredFields();
-					fieldGeneration(fields, cf);
 					// get all methods that should be mutate
 					CtMethod[] methods = ctClass.getDeclaredMethods();
 					codeGeneration(code, mc, methods, cf);
 				}
 			}
 		}
-
+		jar.close();
 	}
 	
-	/**
-	 * there is no mutation here
-	 * @param fields
-	 * @param cf
-	 * @throws BadBytecode
-	 * @throws NotFoundException
-	 * @throws IOException
-	 * @throws CannotCompileException
-	 */
-	private void fieldGeneration (CtField[] fields, ClassFile cf) throws BadBytecode, NotFoundException, IOException, CannotCompileException {
-		for (CtField field : fields) {
-			FieldInfo fi = new FieldInfo(cf.getConstPool(), field.getFieldInfo().getName(), field.getFieldInfo().getDescriptor());
-			fi.setAccessFlags(field.getFieldInfo().getAccessFlags());
-			cf.addField(fi);
-		}
-	}
 
 	/**
 	 * 
@@ -106,13 +90,12 @@ public class Mutator {
 	 */
 	private void codeGeneration(Bytecode code, MutateClass mc, CtMethod[] methods, ClassFile cf) throws BadBytecode, DuplicateMemberException {
 		for (CtMethod method : methods) {
-			MethodInfo mi = new MethodInfo(cf.getConstPool(), method.getMethodInfo().getName() , method.getMethodInfo().getDescriptor());
 			// construction des methods a muter
 			MutateMethod mm = new MutateMethod(method.getName());
 			
 			CodeAttribute ca = method.getMethodInfo().getCodeAttribute();
 			
-			codeIterator(code, mc, mm, ca, mi, cf);
+			codeIterator(code, mc, mm, ca, cf);
 			
 			// on remet le code a zero
 			code = new Bytecode(cf.getConstPool());
@@ -128,7 +111,7 @@ public class Mutator {
 	 * @throws BadBytecode
 	 * @throws DuplicateMemberException 
 	 */
-	private void codeIterator(Bytecode code, MutateClass mc, MutateMethod mm, CodeAttribute ca, MethodInfo mi, ClassFile cf) throws BadBytecode, DuplicateMemberException {
+	private void codeIterator(Bytecode code, MutateClass mc, MutateMethod mm, CodeAttribute ca, ClassFile cf) throws BadBytecode, DuplicateMemberException {
 		if (ca != null) {
 			CodeIterator ci = ca.iterator();
 			while (ci.hasNext()) {
@@ -147,9 +130,6 @@ public class Mutator {
 					//System.out.println("c'est une addition");
 				}
 			}
-			// we have to set codeAttribute
-			mi.setCodeAttribute(code.toCodeAttribute());
-			cf.addMethod(mi);
 			
 			//System.out.println("le code copié :" + code.toString());
 			
@@ -162,15 +142,6 @@ public class Mutator {
 	}
 	
 	/**
-	 * 
-	 * @param name
-	 * @return
-	 */
-	private static ClassFile createClassFile(String nameClass, String superClassName) {
-		return superClassName == null ? new ClassFile(false, nameClass, null) : new ClassFile(false, nameClass, superClassName);
-		
-	}
-	/**
 	 * If the op must be mutate, then b will be added to it with the mutated op
 	 * @param b
 	 * @param op
@@ -180,25 +151,4 @@ public class Mutator {
 		b.add(op);
 	}
 	
-	private ClassFile initClassFile(CtClass ct) {
-		ClassFile cf = createClassFile(ct.getName(), null);
-		
-		try {
-			CtClass superClass = ct.getSuperclass();
-			
-			cf.setSuperclass(superClass.getName());
-			
-			CtClass[] interfaceClass = ct.getInterfaces();
-			
-			
-			for(CtClass ic : interfaceClass) {
-				cf.addInterface(ic.getName());
-			}
-			return cf;
-		} catch (NotFoundException e) {
-			return cf;
-		} catch (CannotCompileException e) {
-			return cf;
-		} 
-	}
 }
